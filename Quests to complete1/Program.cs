62e335c630fc5b5ab0200142e1c11e893272a82d
Cyruss Sese <ac117370@avcol.school.nz>
2021-05-27 12:29:25 +1200

    added while loops in the  quests section

--- Quests to complete1/Program.cs
+++ Quests to complete1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Quests_to_complete1
 {
@@ -7,8 +8,11 @@ namespace Quests_to_complete1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to your Quests To Complete!");
-            Console.WriteLine("(press any key to continue)");
+            Console.WriteLine("(press any key to continue)\n");
             Console.ReadKey();
+            Console.WriteLine("Can I get your name?"); //the user will be asked to comment his/her name.
+
+            Console.Write("Name:"); string name1 = Console.ReadLine();
             Console.WriteLine("--------------------------------------------");
             intro1();
 
@@ -16,12 +20,72 @@ namespace Quests_to_complete1
         }
         public static void intro1()
         {
+            string quests1 = "Do the Dishes";
+            string quests2 = "Do the Dishes";
+            string quests3 = "Do the Dishes";
+
+            File.WriteAllText("filename.txt", quests1);
+            File.WriteAllText("filename.txt", quests2);
+            File.WriteAllText("filename.txt", quests3);
+
+
             Console.WriteLine("Do you want to look to your Quests for today?");
             Console.Write("Answer:"); string response1 = Console.ReadLine().ToLower();
             Console.WriteLine("--------------------------------------------");
-            if (response1 == "yes")
+            if (response1 == "yes")//if the user say 'yes', the program will show the quests below
             {
-                Console.WriteLine("");
+                Console.WriteLine("These are your Quests for today...");//lists of quests are shown below
+                Console.WriteLine("A." +quests1);
+                Console.WriteLine("B." +quests2);
+                Console.WriteLine("C." +quests3);
+                Console.WriteLine("Do you want to edit your quests?");
+                Console.WriteLine("Press the letter of the quest you want to edit or delete");
+                Console.WriteLine("(example: A,a )");
+                Console.Write("Command:"); string command1 = Console.ReadLine().ToLower();
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine("----------------------------------------------------------------");
+                if (command1 == "a")
+                {
+                    Console.WriteLine("What do you want to do to this Quest?");
+                    Console.WriteLine("(comment 'remove' if you want to delete the quest or comment 'edit' if you want to edit or replace the quest\a");
+                    Console.WriteLine("(or if you want to go back to your quests, comment 'mainmenu'.)");
+                    Console.Write("Command:"); string commandanswer1 = Console.ReadLine().ToLower();
+                    string mainmenu = "mainmenu";
+                    string remove = "remove";
+                    string edit = "edit";
+                    while (commandanswer1 != mainmenu && commandanswer1 != remove && commandanswer1 != edit)
+                    {
+                        Console.WriteLine("Please comment a valid command");
+                        Console.Write("Command:"); commandanswer1 = Console.ReadLine();
+                    }
+                    //
+                    //
+                    //
+                    if (commandanswer1 == "mainmenu")
+                    {
+                        Console.Clear();
+                        intro1();
+                    }
+                    else if (commandanswer1 == "remove")
+                    {
+                        File.Delete(quests1);
+                        Console.WriteLine("Command Complete");
+                        Console.WriteLine("(press any key to continue");
+                        Console.ReadKey();
+                        Console.Clear();
+                        intro1();
+                    }
+                }
+                else if (command1 == "b")
+                {
+
+                }
+                else if (command1 == "c")
+                {
+
+                }
+                
             }
             else
 
@@ -46,7 +110,6 @@ namespace Quests_to_complete1
             Console.Clear();
             Console.WriteLine("OK, have a good day :)");
             Console.WriteLine("(press any key to continue)");
-            Console.ReadKey();
         }
     }
 }
